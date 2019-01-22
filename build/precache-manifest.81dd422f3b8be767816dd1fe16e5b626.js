a7af234f563099a524519cb18cd248ba2aec2952
bowen-wu <13793248162@163.com>
2019-01-22 19:57:40 +0800

    feat(): build

---
+++ build/precache-manifest.81dd422f3b8be767816dd1fe16e5b626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3d85f5a9f9f9ac52fd5",
+    "url": "/static/js/main.f3d85f5a.chunk.js"
+  },
+  {
+    "revision": "5a6cd0ba9702ce8b5052",
+    "url": "/static/js/1.5a6cd0ba.chunk.js"
+  },
+  {
+    "revision": "f3d85f5a9f9f9ac52fd5",
+    "url": "/static/css/main.eb900c92.chunk.css"
+  },
+  {
+    "revision": "5a6cd0ba9702ce8b5052",
+    "url": "/static/css/1.7b1f8a30.chunk.css"
+  },
+  {
+    "revision": "80f181b4d7092005aadd79464e24271b",
+    "url": "/index.html"
+  }
+];
