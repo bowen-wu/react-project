da722ff8827d5a99a0f846b52a70cc91dc951379
bowen-wu <13793248162@163.com>
2019-01-22 19:34:27 +0800

    feat(): use scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Button } from 'antd-mobile';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <img src={logo} />
                 <Button>default</Button>
                 <Button type="primary">primary</Button>
             </div>
