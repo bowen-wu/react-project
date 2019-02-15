7bb1f9bcdf95de2ea28c1582e375d394da120c6c
bowen-wu <13793248162@163.com>
2019-02-15 15:36:52 +0800

    feat(Error): complete Error page

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -6,6 +6,8 @@ import Util from '../../common/utils/util';
 import Loading from '../../components/Loading/Loading';
 
 import './Error.scss';
+import ErrorIcon from '../../common/img/404.svg';
+
 
 class Error extends Component {
     constructor() {
@@ -17,10 +19,9 @@ class Error extends Component {
     }
     render() {
         return (
-            <div className="title">
-                Error page
-                <hr />
-                <Button onClick={this.goBackToHome.bind(this)}>go back to Home</Button>
+            <div className="error">
+                <img src={ErrorIcon} className='error-icon' alt='404' />
+                <div className='error-tip'>404！抱歉，您查看的页面不存在～～</div>           
             </div>
         )
     }
